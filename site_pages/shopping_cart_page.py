588604dce315a6ee5482ff60f0aa4543a64abd26
Priven <privenmalwane@gmail.com>
2024-05-08 21:09:37 +0200

    updated tests

--- site_pages/shopping_cart_page.py
+++ site_pages/shopping_cart_page.py
@@ -1,17 +1,17 @@
 from site_pages.base_page import BasePage
 from playwright.sync_api import Page
 
-class InventoryPage(BasePage):
+class ShoppingPage(BasePage):
     def __init__(self, page: Page):
         self.page = page
-
-    def get_quantity_text(self):
-        OnesireText = self.page.locator("#cart_contents_container > div > div.cart_list > div.cart_item > div.cart_quantity")
-        return OnesireText
-    
+  
     def get_price_text(self):
-        OnesireText = self.page.locator("#cart_contents_container > div > div.cart_list > div.cart_item > div.cart_item_label > div.item_pricebar > div")
-        return OnesireText
+        get_unitprice = self.page.locator("[data-test=\"inventory-item-price\"]")
+        return get_unitprice
     
-    def click_add_checkout(self):
-        self.page.locator("#checkout").click()
+    def get_quantity_in_cart(self):
+        get_quantity = self.page.locator("[data-test=\"item-quantity\"]")
+        return get_quantity
+
+    def checkout_product(self):
+        self.page.locator("[data-test=\"checkout\"]").click()
