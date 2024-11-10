3abcd7b41246f0875e431d2490ff1fe02c86f609
Priven <privenmalwane@gmail.com>
2024-11-10 20:48:06 +0200

    update

--- tests/functional/test_file.py
+++ tests/functional/test_file.py
@@ -3,7 +3,6 @@ from utils.playwright_manager import launch_browser
 from utils.config import baseUrl
 from test_data.login_test_data import login_test_data
 from test_data.select_item_test_data import select_item_testdata
-from test_data.checkout_test_data import checkout_test_data
 from test_data.cart_test_data import cart_test_data
 from playwright.sync_api import expect
 import logging
